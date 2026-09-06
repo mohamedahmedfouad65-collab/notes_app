d4a541e4c18fa1bf0263bd4b77c1aee3674ebdd9
Mohamed Fouad <mohamedahmedfouad65@gmail.com>
2026-09-06 18:48:24 +0300

    create edit note view

--- lib/views/widgets/add_note_button_sheet.dart
+++ lib/views/widgets/add_note_button_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_filed.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -7,13 +8,18 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          const CustomTextFiled(hint: 'Title'),
-          SizedBox(height: 16),
-          CustomTextFiled(hint: 'Content', maxLins: 4),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            const CustomTextFiled(hint: 'Title'),
+            SizedBox(height: 16),
+            CustomTextFiled(hint: 'Content', maxLins: 4),
+            SizedBox(height: 65),
+            customBottum(),
+            SizedBox(height: 24),
+          ],
+        ),
       ),
     );
   }
--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,47 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/widgets/edit_note_view.dart';
 
 class notesItem extends StatelessWidget {
   const notesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
 
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Flutter is a framework for building mobile applications.',
+                  style: TextStyle(color: Colors.black.withValues(alpha: 0.5)),
+                ),
+              ),
+
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(FontAwesomeIcons.trash, color: Colors.black),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 24, top: 16),
               child: Text(
-                'Flutter is a framework for building mobile applications.',
+                'May,21 ,2022',
                 style: TextStyle(color: Colors.black.withValues(alpha: 0.5)),
               ),
             ),
-
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(FontAwesomeIcons.trash, color: Colors.black),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24, top: 16),
-            child: Text(
-              'May,21 ,2022',
-              style: TextStyle(color: Colors.black.withValues(alpha: 0.5)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
