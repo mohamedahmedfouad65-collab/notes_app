241f96d6b4fbb16974e9d9da759f736a34c68b59
Mohamed Fouad <mohamedahmedfouad65@gmail.com>
2026-09-07 04:03:38 +0300

    create note model

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,13 @@
+class noteModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  noteModel({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.color,
+  });
+}
